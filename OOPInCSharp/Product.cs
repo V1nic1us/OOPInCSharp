8337ec0cc49f037a54ab371d37e8d5ef9ed45e68
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-10-08 02:06:14 -0400

    feat: feat: class creation and data insertion

---
+++ OOPInCSharp/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPInCSharp
+{
+    internal class Product
+    {
+        public string Name;
+        public double Price;
+        public int Amount;
+
+        public Product(string name, double price, int amount) 
+        { 
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+
+        public double TotalValueInStock() {
+            return Price * Amount;
+        }
+
+        public void AddProduct(int amount)
+        {
+            Amount += amount;
+        }
+
+        public void RemoveProduct(int amount)
+        {
+            Amount -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Produto: {Name}, R$ {Price}, Quantidade: {Amount}, Valor Total no Estoque: {TotalValueInStock()}";
+        }
+    }
+}
---
+++ OOPInCSharp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OOPInCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do Produto");
+            string name = Console.ReadLine();
+            Console.WriteLine("Digite o preço");
+            double price = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade");
+            int amount = int.Parse(Console.ReadLine());
+
+            Product product = new Product(name, price, amount);
+            PrintProduct(product);
+
+            Console.WriteLine("Digite a quantidade para ser adicionado");
+            amount = int.Parse(Console.ReadLine());
+            PrintProduct(product);
+            Console.WriteLine("Digite a quantidade para ser retirada");
+            amount = int.Parse(Console.ReadLine());
+            PrintProduct(product);
+
+        }
+
+        static void PrintProduct(Product product)
+        {
+            Console.WriteLine();
+            Console.WriteLine(product);
+            Console.WriteLine();
+        }
+    }
+}
